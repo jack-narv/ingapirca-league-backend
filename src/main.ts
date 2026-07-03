85c095c8744459352012c227bcc550475e5278e5
Jack <jackricardonarvaezsalazar@gmail.com>
2026-07-02 21:48:32 -0500

    Bug top strickers fix

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ process.env.TZ = 'UTC';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableShutdownHooks();
   app.use(morgan('dev'));
   await app.listen(process.env.PORT ?? 3000);
 }
